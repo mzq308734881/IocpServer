141033ac15115d228b9fcab8dba5fd5fed7f2816
明章强 <308734881@qq.com>
2017-04-24 22:01:24 +0800

    IocpServer
    完成端口服务端

---
+++ IocpServer/Client/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace IocpClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnectOne = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConnectMore = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnConnectOne
+            // 
+            this.btnConnectOne.Location = new System.Drawing.Point(306, 26);
+            this.btnConnectOne.Name = "btnConnectOne";
+            this.btnConnectOne.Size = new System.Drawing.Size(75, 23);
+            this.btnConnectOne.TabIndex = 0;
+            this.btnConnectOne.Text = "单个连接";
+            this.btnConnectOne.UseVisualStyleBackColor = true;
+            this.btnConnectOne.Click += new System.EventHandler(this.btnConnectOne_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // btnConnectMore
+            // 
+            this.btnConnectMore.Location = new System.Drawing.Point(306, 65);
+            this.btnConnectMore.Name = "btnConnectMore";
+            this.btnConnectMore.Size = new System.Drawing.Size(75, 23);
+            this.btnConnectMore.TabIndex = 0;
+            this.btnConnectMore.Text = "多个连接";
+            this.btnConnectMore.UseVisualStyleBackColor = true;
+            this.btnConnectMore.Click += new System.EventHandler(this.btnConnectMore_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "6000";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(19, 131);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(258, 172);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 335);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnectMore);
+            this.Controls.Add(this.btnConnectOne);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnectOne;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConnectMore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ IocpServer/Client/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace IocpClient
+{
+    public partial class Form1 : Form
+    {
+        private Socket socketClient;
+        private Thread threadClient;
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnConnectOne_Click(object sender, EventArgs e)
+        {
+            string ip = textBox1.Text.Trim();
+            int port = Convert.ToInt32( textBox2.Text.Trim());
+            ClientConnect(ip,port);
+        }
+
+        private void btnConnectMore_Click(object sender, EventArgs e)
+        {             
+            string ip = textBox1.Text.Trim();
+            int port = Convert.ToInt32(textBox2.Text.Trim());
+            for (int i = 0; i < 99; i++)
+            {
+                ClientConnect(ip, port);
+            }
+        }
+
+        #region 连接服务端方法
+        private void ClientConnect(string IP, int Port)
+        {
+            //定义一个套字节监听  包含3个参数(IP4寻址协议,流式连接,TCP协议)
+            socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //需要获取文本框中的IP地址
+            IPAddress ipaddress = IPAddress.Parse(IP);
+            //将获取的ip地址和端口号绑定到网络节点endpoint上
+            IPEndPoint endpoint = new IPEndPoint(ipaddress, Port);
+
+            //这里客户端套接字连接到网络节点(服务端)用的方法是Connect 而不是Bind
+            socketClient.Connect(endpoint);
+
+            //创建一个线程 用于监听服务端发来的消息
+            threadClient = new Thread(showClient);
+            listBox1.Items.Add(socketClient.RemoteEndPoint);
+            //将窗体线程设置为与后台同步
+            threadClient.IsBackground = true;
+
+            //启动线程
+            threadClient.Start();
+        }
+        #endregion
+
+        public void showClient()
+        {
+           //listBox1.Items.Add("11");
+        }
+    }
+}
